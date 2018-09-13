7c75a33004ea6238133f7bbb09abfc11c3e0f665
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-09-13 15:45:19 +0530

    HTTP: Define the heroesUrl with the address of the heroes resource on the server.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
@@ -9,7 +9,12 @@ import { MessageService } from './message.service';
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
@@ -22,4 +27,10 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
 }
